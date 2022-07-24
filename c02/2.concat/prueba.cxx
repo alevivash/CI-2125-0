dd6f762f1091dbc061031ab2519ee1738352b212
TIvink <alejandrovivash5@gmail.com>
2022-07-24 17:51:04 -0400

    actualizacion

---
+++ c02/2.concat/prueba.cxx
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char *concatenate(const char *lhs, const char *rhs) {
+  size_t lsize = strlen(lhs);
+  size_t rsize = strlen(rhs);
+  
+  char *result = (char * ) malloc((lsize + rsize + 1) * sizeof(char));
+  strcpy(result,lhs);
+  strcpy(&result[lsize],rhs);
+
+
+  // ... completar
+}
+
+int main(void)
+{
+  const char* perro ="perro";
+  const char* gato ="gato";
+  
+  const char* text1[] = {"arroz", "pasta", "carne", "fruta"};
+  const char* text2[] = {"amarillo", "morado", "rojo", "azul"};
+
+  const char *result  = concatenate(perro, gato);
+
+  printf("%s", result);
+}
